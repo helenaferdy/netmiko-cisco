bf9fa509152a56271a1584a39627f71016424136
Sena <36813961+helenaferdy@users.noreply.github.com>
2023-06-30 15:01:28 +0700

    final topology

--- libs/NetworkTopology/main.py
+++ libs/NetworkTopology/main.py
@@ -1,34 +1,35 @@
 from pyvis.network import Network
-from lib.NetworkTopology.device import Device
+from libs.NetworkTopology.device import Device
 import csv
 import os
-import sys
+import re
 
 CDP_PATH = "out/getCDP/"
-CURRENT_PATH = "lib/NetworkTopology/"
+CURRENT_PATH = "libs/NetworkTopology/"
 DEVICE_CSV = CURRENT_PATH+"files/device.csv"
 FINAL_HTML = CURRENT_PATH+"files/index.html"
-PYVIS_LIB = CURRENT_PATH+"lib"
-sys.path.append(PYVIS_LIB)
-
 
 def main():
     cdp_file = get_cdp()
-    extract_cdp(cdp_file)
-    # create_topology()
+    if cdp_file != "":
+        extract_cdp(cdp_file)
+        create_topology()
 
 def get_cdp():
     cdp_list = []
-
-    for root, dirs, files in os.walk(CDP_PATH):
-        for file in files:
-            if file.startswith('.'):
-                pass
-            else:
-                the_cdp = os.path.join(root, file)
-                cdp_final = the_cdp.replace("out/getCDP/", "")
-                cdp_list.append(cdp_final)
-
+    try:
+        for root, dirs, files in os.walk(CDP_PATH):
+            for file in files:
+                if file.startswith('.'):
+                    pass
+                else:
+                    the_cdp = os.path.join(root, file)
+                    cdp_final = the_cdp.replace("out/getCDP/", "")
+                    cdp_list.append(cdp_final)
+    except:
+        print("No CDP file found. \nRun the Show CDP Menu first.")
+        return ""
+    
     if cdp_list == []:
         print("No CDP file found. \nRun the Show CDP Menu first.")
         return ""
@@ -42,17 +43,28 @@ def get_cdp():
         return cdp_file
 
 def extract_cdp(cdp_file):
+    columns = ["Local Hostname", "Remote Hostname", "Local Interface"]
+    replace = ["\(", ".bankmayapada.", ".primacom."]
+
     input_file = f"{CDP_PATH}{cdp_file}"
     output_file = DEVICE_CSV
-    columns = ["Local Hostname", "Remote Hostname", "Local Interface"]
 
     with open(input_file, 'r') as input_file:
         reader = csv.DictReader(input_file)
         extracted_data = []
+
         for row in reader:
+            source = row[columns[0]]
+            target = row[columns[1]]
+            for r in replace:
+                # source = source.replace(r, "")
+                source = re.sub(f'{r}.*','', source)
+                # target = target.replace(r, "")
+                target = re.sub(f'{r}.*','', target)
+
             extracted_row = {
-                "source": row[columns[0]],
-                "target": row[columns[1]],
+                "source": source,
+                "target": target,
                 "weight": 1,
                 "int": row[columns[2]]
             }
--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-from lib.getCPU.main import main as CPU
-from lib.getInven.main import main as Inven
-from lib.getEnvi.main import main as Envi
-from lib.getCustom.main import main as Custom
-from lib.getCDP.main import main as CDP
-from lib.getCRC.main import main as CRC
-from lib.NetworkTopology.main import main as Topology
+# from lib.getCPU.main import main as CPU
+# from lib.getInven.main import main as Inven
+# from lib.getEnvi.main import main as Envi
+# from lib.getCustom.main import main as Custom
+# from lib.getCDP.main import main as CDP
+# from lib.getCRC.main import main as CRC
+from libs.NetworkTopology.main import main as Topology
 
 
 # CPU()
